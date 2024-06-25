41980e5de978e2a3df6c8dfed7e931cab0457563
LauroEmm <lauro.rafael.ismart@gmail.com>
2024-06-25 15:20:26 -0300

    feat: add readByUser

--- src/Controllers/CertificateController.js
+++ src/Controllers/CertificateController.js
@@ -1,6 +1,7 @@
 import CertificateModel from "../Models/CertificateModel.js";
 import UserModel from "../Models/UserModel.js";
 import TreeModel from "../Models/TreeModel.js";
+import moment from "moment";
 
 class CertificateController {
   async create(req, res) {
@@ -27,11 +28,10 @@ class CertificateController {
     }
   }
 
-  async read(req, res) {
+  async readAll(req, res) {
     try {
       const certificate = await CertificateModel.find(req.body);
-      // .populate("id_tree")
-      // .populate("id_user");
+
       return res.status(200).json(certificate);
     } catch (error) {
       res.status(500).json({ message: "ERROR", error: error.message });
@@ -62,6 +62,34 @@ class CertificateController {
       if (!foundCertificate) return res.status(404).json({ message: "Certificate not found!" });
       const certificate = await foundCertificate.set(req.body).save();
       res.status(200).json(certificate);
+    } catch (error) {
+      res.status(500).json({ message: "ERROR", error: error.message });
+    }
+  }
+
+  async readByUser(req, res) {
+    try {
+      const { id } = req.params;
+      const certificated = await CertificateModel.find({ id_user: id });
+
+      let { type } = req.query;
+      let filteredCertificates;
+
+      switch (type) {
+        case "expirated":
+          filteredCertificates = certificated.filter((certificate) => {
+            return moment(certificate.expirateDate).isBefore(moment());
+          });
+          break;
+        case "active":
+          filteredCertificates = certificated.filter((certificate) => {
+            return moment(certificate.expirateDate).isAfter(moment());
+          });
+          break;
+        default:
+          filteredCertificates = certificated;
+      }
+      return res.status(200).json(filteredCertificates);
     } catch (error) {
       res.status(500).json({ message: "ERROR", error: error.message });
     }
--- src/routes/CertificateRoutes.js
+++ src/routes/CertificateRoutes.js
@@ -7,10 +7,12 @@ const certificateRoutes = Router();
 certificateRoutes
   .route("/")
   .post(CertificateValidator.create, CertificateController.create)
-  .get(CertificateController.read);
+  .get(CertificateController.readAll);
 
 certificateRoutes
   .route("/:id")
+  .get(CertificateController.readByUser)
   .delete(CertificateValidator.destroy, CertificateController.destroy)
   .put(CertificateValidator.update, CertificateController.update);
+
 export default certificateRoutes;
