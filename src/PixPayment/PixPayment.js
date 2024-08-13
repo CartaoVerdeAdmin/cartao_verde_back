3380c88761090f43bec951ac49d323f3d01bfb7e
Otavio <otavioaugustobr@gmail.com>
2024-08-13 00:08:16 -0300

    fix: finishing PIX backend

--- src/PixPayment/PixPayment.js
+++ src/PixPayment/PixPayment.js
@@ -1,34 +1,39 @@
 import { MercadoPagoConfig, Payment } from "mercadopago";
+import { v4 as uuidv4 } from "uuid";
 
 class PixPayment {
   async create(req, res) {
     const client = new MercadoPagoConfig({
-      accessToken: process.env.ACESSTOKEN,
-      options: { timeout: 5000 },
+      accessToken: "TEST-3618565473876869-080617-de83334492688097bbaeb92c9dcb79cf-494462802",
+      options: { timeout: 5000, idempotencyKey: "abc" },
     });
     const payment = new Payment(client);
 
     const data = req.body;
 
-    const teste = payment
-      .create({
-        body: {
-          transaction_amount: data.transaction_amount,
-          description: data.description,
-          payment_method_id: "pix",
-          payer: {
-            email: data.email,
-            identification: {
-              type: "CPF",
-              number: data.number,
-            },
-          },
+    const body = {
+      transaction_amount: data.transaction_amount,
+      description: data.description,
+      payment_method_id: "pix",
+      payer: {
+        email: data.email,
+        identification: {
+          type: "CPF",
+          number: data.number,
         },
-        requestOptions: { idempotencyKey: "<SOME_UNIQUE_VALUE>" },
-      })
-      .then((result) => console.log(result))
-      .catch((error) => console.log(error));
-    return res.status(200).json("link gerado com sucesso");
+      },
+    };
+
+    const requestOptions = { idempotencyKey: uuidv4() };
+
+    try {
+      const result = await payment.create({ body, requestOptions });
+      res.status(200).json(result.point_of_interaction.transaction_data.ticket_url);
+      return result.point_of_interaction.transaction_data.ticket_url;
+    } catch (error) {
+      console.error(error);
+      return res.status(400).json(error);
+    }
   }
 }
 
